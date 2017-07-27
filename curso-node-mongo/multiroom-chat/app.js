27de6f157219ffa51f3fb2194151a4b373780c32
Paulo Henrique Pereira Santana <paulinhps@hotmail.com>
2017-07-27 18:07:35 -0300

    primeira aula para criar o app multiroom-chat

---
+++ curso-node-mongo/multiroom-chat/app.js
@@ -0,0 +1,7 @@
+var app = require("./config/server");
+
+app.listen(1447, () => {
+
+    console.log('Servidor Escultando na porta 1447');
+}
+);
---
+++ curso-node-mongo/multiroom-chat/config/server.js
@@ -0,0 +1,33 @@
+/* importar o módulo do framework express */
+const express = require('express');
+
+/* importar o módulo do framework consign */
+const consign = require('consign');
+
+/* importar o middle body-parser */
+const bodyParser = require('body-parser');
+
+/* importar o middle express-validator */
+const expressValidator = require('express-validator');
+
+/* iniciar o express */
+var app = express();
+
+/* setar as variaveis de 'view engine' e 'views' do express */
+app.set('view engine', 'ejs');
+app.set('views', '../app/views');
+
+/* configurar os middles do app */
+app.use(express.static('../app/public'));
+app.use(bodyParser.urlencoded({extended : true})); 
+app.use(expressValidator());
+
+/* configurar o consign */
+consign().include('app/routes')
+         .then('app/models')
+         .then('app/controllers')
+         .into(app);
+
+
+/* exportar o express */
+module.exports = app;
